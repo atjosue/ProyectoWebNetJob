79436920467ece9dc6300792b2b723dfe293b404
CarlosCampos97 <31112649+CarlosCampos97@users.noreply.github.com>
2018-10-28 21:24:54 -0600

    Dao y Clase Usuario

---
+++ src/java/com/modelos/Usuario.java
@@ -0,0 +1,22 @@
+
+package com.modelos;
+
+/**
+ * Nombre de la clase: Usuario
+ * Fecha: 20/02/2018
+ * Version 1.0
+ * copyRight 
+ * @author Carlos Campos
+ */
+public class Usuario {
+    
+    
+    private int idUsuario;
+    private String nombreUsuario;
+    private String contraseña;
+    private int estado;
+    private int idRol;
+    
+    
+    
+}
